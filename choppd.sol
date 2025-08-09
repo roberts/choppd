1bf0d279622b0b64ab2d3e48f1341785930c89e6
Drew Roberts <github@drewroberts.com>
2025-08-09 01:22:47 -0400

    Update tax wallet addys

--- choppd.sol
+++ choppd.sol
@@ -1226,9 +1226,9 @@ contract ChoppdBase is ERC20, Ownable {
             marketingLopsidedSellTax +
             developerLopsidedSellTax;
 
-        communityWallet = address(0xC6aa2f0FF6b8563EA418ec2558890D6027413699); // Community Funds
-        marketingWallet = address(0xC6aa2f0FF6b8563EA418ec2558890D6027413699); // Marketing Funds
-        developerWallet = address(0xA6d26E99660de4974B8994eCF75dcD4Cf34951B6); // Developer Funds
+        communityWallet = address(0x6871552694dBC55a1E6386CA9424ad3b4742C0ea); // Community Funds
+        marketingWallet = address(0xd98014C3c962B0E89c461dc22D7fFa98987522d4); // Marketing Funds
+        developerWallet = address(0xDEB333a3240eb2e1cA45D38654c26a8C1AAd0507); // Developer Funds
 
         _mint(address(this), totalSupply);
 
